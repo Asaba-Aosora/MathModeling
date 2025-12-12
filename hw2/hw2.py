83c1e8d2ba035afd2ea2e0117462efecd3d6af4a
Asaba_Aosora <2737169859@qq.com>
2025-12-12 21:01:16 +0800

    重建和可视化也做出来了

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -27,13 +27,50 @@ eigenvalues2, eigenvectors2 = PCA_process(faces_non_centered)
 def plot_eigenfaces(eigenvectors, title_prefix):
     plt.figure(figsize=(10, 10))
     for i in range(10):
-        eigenface = eigenvectors[:, i].reshape(img_h, img_w)
+        # pytorch张量是行优先, 但是matlab是列优先, 这里必须转置
+        eigenface = eigenvectors[:, i].reshape(img_w, img_h).t()
         plt.subplot(2, 5, i+1)
         plt.imshow(eigenface, cmap='gray')
-        plt.title(f'{title_prefix}\npic{i+1}')
+        plt.title(f'{title_prefix}\nPC{i+1}')
         plt.axis('off')
     plt.tight_layout()
     plt.show()
 
 plot_eigenfaces(eigenvectors1, 'Centered')
 plot_eigenfaces(eigenvectors2, 'Non-centered')
+
+def reconstruct(X, mean, eigenvectors, dim):
+    components = eigenvectors[:, :dim]      # 按要求提取前dim个特征向量
+    projected = X @ components
+    reconstructed = projected @ components.T
+    if mean is not None:
+        reconstructed += mean.squeeze()
+    reconstructed = torch.clamp(reconstructed, 0, 255)
+    return reconstructed.reshape(img_w, img_h).t()
+
+original_face = faces[0].reshape(img_w, img_h).t()
+
+recon_centered_7 = reconstruct(faces_centered[0], mean, eigenvectors1, 7)
+recon_centered_54 = reconstruct(faces_centered[0], mean, eigenvectors1, 54)
+recon_non_centered_7 = reconstruct(faces_non_centered[0], None, eigenvectors2, 7)
+recon_non_centered_54 = reconstruct(faces_non_centered[0], None, eigenvectors2, 54)
+
+def plot_recon(original, recon_7, recon_54, title_prefix):
+    plt.figure(figsize=(10, 10))
+    plt.subplot(1, 3, 1)
+    plt.imshow(original, cmap='gray')
+    plt.title("Original")
+    plt.axis('off')
+    plt.subplot(1, 3, 2)
+    plt.imshow(recon_7, cmap='gray')
+    plt.title(f"{title_prefix} 7 dim")
+    plt.axis('off')
+    plt.subplot(1, 3, 3)
+    plt.imshow(recon_54, cmap='gray')
+    plt.title(f"{title_prefix} 54 dim")
+    plt.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+plot_recon(original_face, recon_centered_7, recon_centered_54, 'Centered')
+plot_recon(original_face, recon_non_centered_7, recon_non_centered_54, 'Non-centered')
